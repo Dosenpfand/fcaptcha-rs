1e7242d380d8f776d07f9f609f32aabb7f2dfdb1
Dosenpfand <m@sad.bz>
2023-08-30 18:24:23 +0200

    Use multiple inputs

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,10 +1,22 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use fcaptcha::build_puzzle;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| {
-        b.iter(|| build_puzzle(black_box("TEST-KEY".as_bytes()), black_box("127.0.0.1")))
-    });
+    let api_key = "b6db8801-4b39-4516-bd74-5eed7d7433a5".as_bytes();
+    let ip_addresses = ["127.0.0.1", "192.168.0.0.1"];
+
+    let mut group = c.benchmark_group("build_puzzle");
+
+    for ip_address in ip_addresses {
+        group.bench_with_input(
+            BenchmarkId::from_parameter(ip_address),
+            ip_address,
+            |b, ip_address| {
+                b.iter(|| build_puzzle(api_key, &ip_address));
+            },
+        );
+    }
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
